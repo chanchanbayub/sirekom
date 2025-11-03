e33af061c34efba28c07a22feddc14c698bcf1ee
chanchanbayub <chanchanbayub@gmail.com>
2025-11-04 00:22:43 +0700

    memperbaiki pemanggilan jquery dan css

--- app/Views/auth/login_v.php
+++ app/Views/auth/login_v.php
@@ -58,7 +58,8 @@
         </div>
     </div>
     <script src="https://code.jquery.com/jquery-3.7.1.js"></script>
-    <script src="/assets/libs/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/simplebar/6.3.2/simplebar.min.js"></script>
+    <!-- <script src="/assets/libs/bootstrap/dist/js/bootstrap.bundle.min.js"></script> -->
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11.10.7/dist/sweetalert2.all.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/iconify-icon@1.0.8/dist/iconify-icon.min.js"></script>
 
